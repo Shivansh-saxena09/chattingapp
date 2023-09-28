8cbc7812adce2868900555f0ec45e786d7292269
Shivansh Saxena <saxenashivansh230@gmail.com>
2023-09-28 12:52:02 +0530

    Initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcrLjC2J5TMFN8OloPGtMX44qIURzB0EI",
+  authDomain: "chattingapp-3e0c9.firebaseapp.com",
+  projectId: "chattingapp-3e0c9",
+  storageBucket: "chattingapp-3e0c9.appspot.com",
+  messagingSenderId: "697772522521",
+  appId: "1:697772522521:web:d9252883ecae385a607e88"
+};
+
+export const app = initializeApp(firebaseConfig);
